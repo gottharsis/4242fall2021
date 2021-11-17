f656fc1e218d2fc9b5a86d26d9ac5a04da3d7369
Ayush Nene <ayush3108@gmail.com>
2021-11-16 19:50:52 -0500

    add documentation to routes

--- server/app.py
+++ server/app.py
@@ -6,9 +6,15 @@ def create_app():
     @app.route("/", defaults = {'path': ''})
     @app.route("/<path:path>")
     def index(path):
+        """
+        Default route: returns the React app
+        """
         return render_template('index.html')
 
     @app.route("/route2")
     def test():
+        """
+        Test route to contrast with server route
+        """
         return "Route 2"
     return app
